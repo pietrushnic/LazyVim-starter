9af03901ca1ee70e3ba7e02372a07c016ccc2373
Piotr Król <piotr.krol@3mdeb.com>
2024-07-12 23:04:50 +0200

    lua/config/keymaps.lua: add keymap for sorting whole buffer
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set({ "n", "v" }, "<leader>bu", "<Cmd>%sort u<CR>", { desc = "Sort whole buffer" })
