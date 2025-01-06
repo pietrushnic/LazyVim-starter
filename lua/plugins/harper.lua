93531861e9edef762b774d1772c177b8f226dfd3
Piotr Król <piotr.krol@3mdeb.com>
2025-01-07 00:51:46 +0100

    lua/plugins/harper.lua: use local disctionaries
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- lua/plugins/harper.lua
+++ lua/plugins/harper.lua
@@ -1,9 +1,15 @@
-local lspconfig = require("lspconfig")
-
-lspconfig.harper_ls.setup({
-  settings = {
-    ["harper-ls"] = {
-      fileDictPath = "~/.config/nvim/spell/",
-    },
-  },
-})
+return {
+  "neovim/nvim-lspconfig",
+  event = "VeryLazy",
+  config = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.harper_ls.setup({
+      settings = {
+        ["harper-ls"] = {
+          fileDictPath = "~/.config/nvim/spell/",
+          userDictPath = "~/.config/nvim/spell/en.utf-8.add",
+        },
+      },
+    })
+  end,
+}
