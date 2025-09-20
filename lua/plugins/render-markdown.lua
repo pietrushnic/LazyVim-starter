2a1444293efc8d9802b111b227e4931faf515a79
Piotr Król <piotr.krol@3mdeb.com>
2025-09-21 01:41:04 +0200

    lua/plugins/render-markdown.lua: disable render-markdown
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,25 +1,4 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  config = function()
-    require("render-markdown").setup({
-      heading = {
-        -- Turn on / off heading icon & background rendering
-        enabled = false,
-      },
-      html = {
-        -- Turn on / off all HTML rendering
-        enabled = true,
-        -- Additional modes to render HTML
-        render_modes = false,
-        comment = {
-          -- Turn on / off HTML comment concealing
-          conceal = false,
-          -- Optional text to inline before the concealed comment
-          text = nil,
-          -- Highlight for the inlined text
-          highlight = "RenderMarkdownHtmlComment",
-        },
-      },
-    })
-  end,
+  enabled = false,
 }
