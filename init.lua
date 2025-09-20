369f4e7009fa4bf8eace03f4f3b2c721cf2f127a
Piotr Król <piotr.krol@3mdeb.com>
2025-09-21 01:39:15 +0200

    init.lua: make sure to disable autoformatting on save
    Signed-off-by: Piotr Król <piotr.krol@3mdeb.com>

--- init.lua
+++ init.lua
@@ -4,9 +4,10 @@ require("config.lazy")
 require("conform").setup({
   format_on_save = function(bufnr)
     -- Disable with a global or buffer-local variable
-    if vim.g.autoformat or vim.b[bufnr].autoformat then
+    if vim.g.autoformat == false or vim.b[bufnr].autoformat == false then
       return
     end
     return { timeout_ms = 500, lsp_fallback = true }
   end,
 })
+
